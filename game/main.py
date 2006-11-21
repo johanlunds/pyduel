74e75c71a9568755309a4f7a1d8d32b87841e8a1
johanlunds <johanlunds@gmail.com>
2006-11-21 18:10:09 +0000

    Ändra mappstruktur, lägga till current working dir i main.py

--- game/main.py
+++ game/main.py
@@ -8,4 +8,6 @@ def main():
    pass
 
 if __name__ == "__main__":
+   # Justera working dir, så spelet funkar oavsett vart man kör det ifrån
+   os.chdir(os.path.abspath(os.path.dirname(sys.argv[0])))
    main()
